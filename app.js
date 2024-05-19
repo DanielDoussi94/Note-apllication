05fcda7810d66040d31342f85607ac428eb3f9a8
Daniel <danieldoussi40@gmail.com>
2024-05-19 05:54:18 +0200

    Creation du fichier main et des fichier ejs

--- app.js
+++ app.js
@@ -5,16 +5,30 @@ const app = express()
 const port = 5000 || process.env.PORT
 require('dotenv').config();
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true})) //encodage des données envoyé par formulaire
 app.use(express.json())
 
 
 // Définition du répertoire contenant les fichiers statiques
-app.use(express.static('public')) //encodage des données envoyé par formulaire
+app.use(express.static('public')) 
+
 
 //Template engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+app.set('layout', 'layouts/main')
+
+app.get('/', function(req, res){
+
+    const locals = {
+        title: 'Node js Notes',
+        description: 'Free node js Notes App'
+    }
+    res.render('index', locals)
+})
+
+// ROUTERS
+app.use('/', require('./server/router/router'))
 
 
 app.listen(port, ()=>{
---
+++ server/controllers/mainController.js
@@ -0,0 +1,19 @@
+// Gestion de la homePage
+exports.homepage = async (req, res) =>{
+    const locals = {
+        title: 'Node js Notes',
+        description: 'Free node js Notes App'
+    }
+    res.render('index', locals)
+}
+
+// Gestion de la homePage
+
+exports.about = async (req, res) =>{
+    const locals = {
+        title: 'Node js Notes',
+        description: 'Free node js Notes App'
+    }
+    res.render('about', locals)
+}
+
